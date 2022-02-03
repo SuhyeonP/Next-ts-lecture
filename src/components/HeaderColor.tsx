abd0a6d771634d7fada2f71e6a89834b1da7aab5
suhyeonP <ahah12k@gmail.com>
2022-02-03 16:33:48 +0900

    :construction: color

--- src/components/HeaderColor.tsx
+++ src/components/HeaderColor.tsx
@@ -1,21 +1,27 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import SubPanelHeader from '@/components/subPanel/SubPanelHeader';
 import ColorPalette from '@/components/Color/ColorPalette/ColorPalette';
 import { HexColorPicker } from 'react-colorful';
+import { useInput } from 'custom-hook-react';
 import { ColorPaletteTitle } from '@/components/Color/ColorPalette/styles';
 import { HeaderColorWrapperStyled } from './styles';
 
 export const linkPaletteColors = ['none', '#FF004F', '#FF8200', '#FAFF00', '#00DE62',
   '#00E3FF', '#0093FF', '#0030CC', '#6C00FF', '#FF00C3'];
 
-export const recentPaletteColors = new Array(10).fill('inherit');
-
 const HeaderColor = (): JSX.Element => {
-  const [highlight, setHighlight] = useState('#FF00C3');
+  const [highlight, onChangeHighLights, setHighlight] = useInput('#FF00C3');
+  const [pickColors, setPickColors] = useState<string[]>(new Array(10).fill('inherit'));
 
   const changeHighlightColors = useCallback((color: string) => {
     setHighlight(color);
   }, []);
+
+  useEffect(() => {
+    if (highlight) {
+      //
+    }
+  }, [highlight]);
 
   return (
     <HeaderColorWrapperStyled>
@@ -32,13 +38,14 @@ const HeaderColor = (): JSX.Element => {
       <div className='recent-color'>
         <ColorPalette
           subTitle='Recent Color'
-          colors={recentPaletteColors}
+          colors={pickColors}
           highlightColor={highlight}
           changeHighLights={changeHighlightColors}
         />
       </div>
       <div style={{ backgroundColor: '#222222' }}>
         <ColorPaletteTitle>More Colors</ColorPaletteTitle>
+        <input type='text' onChange={onChangeHighLights} value={highlight} />
         <HexColorPicker color={highlight} onChange={setHighlight} />
       </div>
     </HeaderColorWrapperStyled>
